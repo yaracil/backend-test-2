a557bd1e9ecb988708e953b6af3301e76e1c77b3
Lenovo <yhacaribe@gmail.com>
2021-02-27 18:57:15 -0600

    Initial project structure

---
+++ mainfunctions/src/main/java/com/example/mainfunctions/MainfunctionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainfunctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainfunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainfunctionsApplication.class, args);
+	}
+
+}
---
+++ mainfunctions/src/test/java/com/example/mainfunctions/MainfunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mainfunctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainfunctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
